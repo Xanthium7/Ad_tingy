877f660650d6d44ece0bc7297658a9d47aad3691
Xanthium7 <akshhaykmurali@gmail.com>
2025-03-07 21:28:36 +0530

    Add web content loading from multiple URLs in generate_embeddings.py

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -1,23 +1,49 @@
 from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader, WebBaseLoader
+import bs4
 
-loaders = [PyPDFLoader('./Adinfo.pdf')]
+urls = [
+    "https://www.nike.com/in/w/mens-shoes-nik1zy7ok",
+    "https://www.nike.com/in/w/womens-shoes-5e1x6zy7ok",
+    "https://www.nike.com/in/w/kids-shoes-1gdj0zy7ok",
 
+]
+
+
+pdf_loaders = [PyPDFLoader('./Adinfo.pdf')]
 docs = []
 
-for file in loaders:
+for file in pdf_loaders:
     docs.extend(file.load())
 
+# Load web content from multiple URLs
+for url in urls:
+    web_loader = WebBaseLoader(
+        web_paths=(url,),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("product-card__title", "product-card__price",
+                        "product-card__description")
+            )
+        ),
+    )
+    web_docs = web_loader.load()
+    print(web_docs)
+    docs.extend(web_docs)
+
+# Split documents into chunks
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=100)
 docs = text_splitter.split_documents(docs)
+
+# Generate embeddings
 embedding_function = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
-
-
+    model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'}
+)
 vectorstore = Chroma.from_documents(
-    docs, embedding_function, persist_directory="./chroma_db_nccn")
+    docs, embedding_function, persist_directory="./chroma_db_nccn"
+)
 
 print(vectorstore._collection.count())
