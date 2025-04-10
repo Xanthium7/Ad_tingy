c9797cef2588884592043d640ea25a96eed72fe5
Xanthium7 <akshhaykmurali@gmail.com>
2025-04-10 23:39:21 +0530

    Add initial implementation of homework assistance agents with OpenAI integration

---
+++ ad_agent.py
@@ -0,0 +1,46 @@
+from agents import Agent, InputGuardrail, GuardrailFunctionOutput, Runner
+from pydantic import BaseModel
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# OpenAI configuration
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+
+
+class HomeworkOutput(BaseModel):
+    is_homework: bool
+    reasoning: str
+
+
+math_tutor_agent = Agent(
+    name="Math Tutor",
+    handoff_description="Specialist agent for math questions",
+    instructions="You provide help with math problems. Explain your reasoning at each step and include examples",
+)
+
+history_tutor_agent = Agent(
+    name="History Tutor",
+    handoff_description="Specialist agent for historical questions",
+    instructions="You provide assistance with historical queries. Explain important events and context clearly.",
+)
+
+triage_agent = Agent(
+    name="Triage Agent",
+    instructions="You determine which agent to use based on the user's homework question",
+    handoffs=[history_tutor_agent, math_tutor_agent],
+
+)
+
+
+async def main():
+    result = await Runner.run(triage_agent, "who was the first president of the united states?")
+    print(result.final_output)
+
+    result = await Runner.run(triage_agent, "what is life")
+    print(result.final_output)
+
+if __name__ == "__main__":
+    asyncio.run(main())
